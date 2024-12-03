e410b9fa10a5628b67b249fc9977013476a5db3d
MrInmortal434 <118313004+MrInmortal@users.noreply.github.com>
2024-12-02 20:06:05 -0400

    Update Producto.cs

--- Modelos/Producto.cs
+++ Modelos/Producto.cs
@@ -1,11 +1,13 @@
-﻿namespace Tienda.Modelos
+namespace Tienda.Modelos
 {
     public class Producto
     {
         public int Id { get; set; }
-        public string Nombre { get; set; } = string.Empty;
-        public string Descripcion { get; set; } = string.Empty;
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
         public decimal Precio { get; set; }
-        public string ImagenURL { get; set; } = string.Empty;
+        public string ImagenURL { get; set; }
+        public string Categoria { get; set; }
     }
+
 }
